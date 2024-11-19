f04483395eaa0564621e131c7c3a5cc4a4deaae2
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-19 16:46:20 -0400

    feat: Improve header information

--- modern-vicidial/agc/vicidial/panel/main.php
+++ modern-vicidial/agc/vicidial/panel/main.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="w-100">
+    <div class="w-100 p-2 text-white">
         <!-- ZZZZZZZZZZZZ  header -->
         <?php
             include 'header.php';
