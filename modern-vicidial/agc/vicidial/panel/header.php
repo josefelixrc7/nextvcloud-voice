e9dfe4f702366a53e43872e92be1cf9a13e89e06
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-19 14:49:02 -0400

    feat: Improve header

--- modern-vicidial/agc/vicidial/panel/header.php
+++ modern-vicidial/agc/vicidial/panel/header.php
@@ -1,4 +1,17 @@
 
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Modern Vicidial</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <div class="navbar-nav">
+        <div class="nav-item text-nowrap">
+        <a class="nav-link px-3" href="#">Cerrar sesi&oacute;n</a>
+        </div>
+    </div>
+</header>
+
 <span id="Header">
 	<input type="hidden" name="extension" id="extension" />
 	<input type="hidden" name="custom_field_values" id="custom_field_values" value="" />
