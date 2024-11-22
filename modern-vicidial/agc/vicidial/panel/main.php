5ae06b440da84be7bc5685e382a2fd835fdb3de4
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-22 11:05:47 -0400

    feat: Improve tabs structure

--- modern-vicidial/agc/vicidial/panel/main.php
+++ modern-vicidial/agc/vicidial/panel/main.php
@@ -132,21 +132,6 @@
                         </div>
                     </li>
                 </ul>
-
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>Saved reports</span>
-                    <a class="link-secondary" href="#" aria-label="Add a new report">
-                        <span data-feather="plus-circle"></span>
-                    </a>
-                </h6>
-                <ul class="nav flex-column mb-2">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <span data-feather="file-text"></span>
-                            Current month
-                        </a>
-                    </li>
-                </ul>
             </div>
         </nav>
         <!-- Barra lateral -->
@@ -202,8 +187,6 @@
             </div>
             <!-- Barra de botones -->
 
-            <h2>Section title</h2>
-        
             <!-- Contents section -->
             <section>
 
--- modern-vicidial/agc/vicidial/panel/tabs.php
+++ modern-vicidial/agc/vicidial/panel/tabs.php
@@ -2,28 +2,87 @@
 <span id="Tabs">
     <table border="0" bgcolor="#FFFFFF" height="30px">
         <tr valign="top" align="left">
-            <td align="left" width="115px" bgcolor="#<?php echo $SSstd_row5_background ?>"><a href="#" onclick="MainPanelToFront('NO','YES');"><img src="/modern-vicidial/images/logo.png" alt="MAIN" height="30px" border="0" /></a></td>
-            <td align="left" width="67px"><a href="#" onclick="ScriptPanelToFront('YES');"><img src="./images/<?php echo _QXZ("vdc_tab_script.gif"); ?>" alt="SCRIPT" width="67px" height="30px" border="0" /></a></td>
-            <?php if ($SSenable_second_script > 0)
-            {echo "<td align=\"left\" width=\"67px\"><a href=\"#\" onclick=\"ScriptPanel2ToFront('YES');\"><img src=\"./images/"._QXZ("vdc_tab_script2.gif")."\" alt=\"SCRIPT 2\" width=\"67px\" height=\"30px\" border=\"0\" /></a></td>\n";}
-            ?>
-            <?php if ($custom_fields_enabled > 0)
-            {echo "<td align=\"left\" width=\"67px\"><a href=\"#\" onclick=\"FormPanelToFront('YES');\"><img src=\"./images/"._QXZ("vdc_tab_form.gif")."\" alt=\"FORM\" width=\"67px\" height=\"30px\" border=\"0\" /></a></td>\n";}
-            ?>
-            <?php if ($email_enabled > 0)
-            {echo "<td align=\"left\" width=\"67px\"><a href=\"#\" onclick=\"EmailPanelToFront('YES');\"><img src=\"./images/"._QXZ("vdc_tab_email.gif")."\" alt=\"EMAIL\" width=\"67px\" height=\"30px\" border=\"0\" /></a></td>\n";}
-            ?>
-            <?php if ($chat_enabled > 0)
+            <td align="left" width="115px" bgcolor="#<?php echo $SSstd_row5_background ?>">
+                <a href="#" onclick="MainPanelToFront('NO','YES');">
+                    <img src="/modern-vicidial/images/logo.png" alt="MAIN" height="30px" border="0" />
+                </a>
+            </td>
+            <td align="left" width="67px">
+                <a href="#" onclick="ScriptPanelToFront('YES');">
+                    <img src="./images/<?php echo _QXZ("vdc_tab_script.gif"); ?>" alt="SCRIPT" width="67px" height="30px" border="0" />
+                </a>
+            </td>
+            <?php
+                //if ($SSenable_second_script > 0)
+                if (true)
                 {
-                # INTERNAL CHAT
-                echo "<td align=\"left\" width=\"67px\"><a href=\"#\" onclick=\"InternalChatContentsLoad('YES');\"><img src=\"./images/"._QXZ("vdc_tab_chat_internal.gif")."\" name='InternalChatImg' alt=\"CHAT\" width=\"67px\" height=\"30px\" border=\"0\"/></a></td>\n";
-
-                # CUSTOMER CHAT
-                echo "<td align=\"left\" width=\"67px\"><a href=\"#\" onclick=\"CustomerChatPanelToFront('1', 'YES');\"><img src=\"./images/"._QXZ("vdc_tab_chat_customer.gif")."\" name='CustomerChatImg' alt=\"CHAT\" width=\"67px\" height=\"30px\" border=\"0\"/></a></td>\n";
+                    echo "
+                        <td align=\"left\" width=\"67px\">
+                            <a href=\"#\" onclick=\"ScriptPanel2ToFront('YES');\">
+                                <img src=\"./images/"._QXZ("vdc_tab_script2.gif")."\" alt=\"SCRIPT 2\" width=\"67px\" height=\"30px\" border=\"0\" />
+                            </a>
+                        </td>\n";
                 }
             ?>
-            <td width="<?php echo $HSwidth ?>px" valign="middle" align="center"><font class="body_tiny">&nbsp; <span id="status"><?php echo _QXZ("LIVE"); ?></span>&nbsp; &nbsp; <?php echo _QXZ("session ID:"); ?> <span id="sessionIDspan"></span></font><br><font class="body_text">&nbsp; &nbsp;<span id="AgentStatusCalls"></span>&nbsp; &nbsp;<span id="AgentStatusEmails"></span></font></td>
-            <td width="109px"><img src="./images/<?php echo _QXZ("agc_live_call_OFF.gif"); ?>" name="livecall" alt="Live Call" width="109px" height="30px" border="0" /></td>
+            <?php
+                //if ($custom_fields_enabled > 0)
+                if (true)
+                {
+                    echo "
+                        <td align=\"left\" width=\"67px\">
+                            <a href=\"#\" onclick=\"FormPanelToFront('YES');\">
+                                <img src=\"./images/"._QXZ("vdc_tab_form.gif")."\" alt=\"FORM\" width=\"67px\" height=\"30px\" border=\"0\" />
+                            </a>
+                        </td>\n";
+                }
+            ?>
+            <?php
+                //if ($email_enabled > 0)
+                if (true)
+                {
+                    echo "
+                        <td align=\"left\" width=\"67px\">
+                            <a href=\"#\" onclick=\"EmailPanelToFront('YES');\">
+                                <img src=\"./images/"._QXZ("vdc_tab_email.gif")."\" alt=\"EMAIL\" width=\"67px\" height=\"30px\" border=\"0\" />
+                            </a>
+                        </td>\n";
+                }
+            ?>
+            <?php
+                //if ($chat_enabled > 0)
+                if (true)
+                {
+                    # INTERNAL CHAT
+                    echo "
+                        <td align=\"left\" width=\"67px\">
+                            <a href=\"#\" onclick=\"InternalChatContentsLoad('YES');\">
+                                <img src=\"./images/"._QXZ("vdc_tab_chat_internal.gif")."\" name='InternalChatImg' alt=\"CHAT\" width=\"67px\" height=\"30px\" border=\"0\"/>
+                            </a>
+                        </td>\n";
+
+                    # CUSTOMER CHAT
+                    echo "
+                        <td align=\"left\" width=\"67px\">
+                            <a href=\"#\" onclick=\"CustomerChatPanelToFront('1', 'YES');\">
+                                <img src=\"./images/"._QXZ("vdc_tab_chat_customer.gif")."\" name='CustomerChatImg' alt=\"CHAT\" width=\"67px\" height=\"30px\" border=\"0\"/>
+                            </a>
+                        </td>\n";
+                }
+            ?>
+            <td width="<?php echo $HSwidth ?>px" valign="middle" align="center">
+                <font class="body_tiny">&nbsp;
+                    <span id="status"><?php echo _QXZ("LIVE"); ?></span>
+                    &nbsp; &nbsp; <?php echo _QXZ("session ID:"); ?>
+                    <span id="sessionIDspan"></span>
+                </font><br>
+                <font class="body_text">&nbsp; &nbsp;
+                    <span id="AgentStatusCalls"></span>&nbsp; &nbsp;
+                    <span id="AgentStatusEmails"></span>
+                </font>
+            </td>
+            <td width="109px">
+                <img src="./images/<?php echo _QXZ("agc_live_call_OFF.gif"); ?>" name="livecall" alt="Live Call" width="109px" height="30px" border="0" />
+            </td>
         </tr>
     </table>
 </span>
