3d6c2d32badaf9cd41930d78cdb0990c19f949a9
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-01 12:14:26 -0400

    feat: Fix DeactivateDOlDSessioNSpan

--- modern-vicidial/agc/vicidial/panel/actions.php
+++ modern-vicidial/agc/vicidial/panel/actions.php
@@ -1107,8 +1107,8 @@ if ($agent_display_dialable_leads > 0)
 	</td></tr></table>
 </span>
 
-<span class="w-100 h-100 bg-white" style="position:absolute;left:0px;top:0px;z-index:<?php $zi++; echo $zi ?>;" id="DeactivateDOlDSessioNSpan">
-    <div class="w-50 ms-auto">
+<span class="w-100 h-100 bg-white sticky-top" style="left:0px;top:0px;z-index:<?php $zi++; echo $zi ?>;" id="DeactivateDOlDSessioNSpan">
+    <div class="w-50 mx-auto">
 		<?php echo _QXZ("Another live agent session was open using your user ID. It has been disabled. Click OK to continue to the agent screen."); ?><br />
 		<a class="btn btn-primary mt-2" href="#" onclick="hideDiv('DeactivateDOlDSessioNSpan');return false;"><?php echo _QXZ("OK"); ?></a>
 	</div>
