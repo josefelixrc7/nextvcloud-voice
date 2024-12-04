942b5bb6bf9477d5c8a07a2033e99d32cc35f243
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-03 23:53:07 -0400

    feat: Improve dispo generation

--- agc/vicidial.php
+++ agc/vicidial.php
@@ -14226,7 +14226,7 @@ function set_length(SLnumber,SLlength_goal,SLdirection)
 								dispo_HTML += `
 									<div class="col-12 col-sm-4 text-center">
 										<div class="p-2 h-100">
-											<a class="btn btn-outline-primary w-100 h-100" href="#" onclick="DispoSelect_submit('','','YES');return false;">
+											<a class="btn btn-outline-secondary w-100 h-100" href="#" onclick="DispoSelect_submit('','','YES');return false;">
 												${VARstatuses[loop_ct]} - ${VARstatusnames[loop_ct]} ${CBflag}
 											</a>
 										</div>
@@ -14238,7 +14238,7 @@ function set_length(SLnumber,SLlength_goal,SLdirection)
 								dispo_HTML += `
 									<div class="col-12 col-sm-4 text-center">
 										<div class="p-2 h-100">
-											<a class="btn btn-outline-primary w-100 h-100" href="#" onclick="DispoSelectContent_create('${VARstatuses[loop_ct]}','ADD','YES');return false;">
+											<a class="btn btn-outline-secondary w-100 h-100" href="#" onclick="DispoSelectContent_create('${VARstatuses[loop_ct]}','ADD','YES');return false;">
 												${VARstatuses[loop_ct]} - ${VARstatusnames[loop_ct]} ${CBflag}
 											</a>
 										</div>
