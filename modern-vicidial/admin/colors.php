88c4c5f10599fea2bf10fdc89c407473f0a37859
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-08 16:15:36 -0400

    feat: Improve table colors

--- modern-vicidial/admin/colors.php
+++ modern-vicidial/admin/colors.php
@@ -3,8 +3,8 @@
     // Main colors
     $SSmenu_background='060e23';
     $SSframe_background='FFF';
-    $SSstd_row1_background='9BB9FB';
-    $SSstd_row2_background='B9CBFD';
+    $SSstd_row1_background='f2f2f2';
+    $SSstd_row2_background='FFF';
     $SSstd_row3_background='8EBCFD';
     $SSstd_row4_background='B6D3FC';
     $SSstd_row5_background='A3C3D6';
