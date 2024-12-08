83e6f7ee83587046816c6f7f860941338c22301a
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-08 16:58:20 -0400

    feat: Improve colors

--- modern-vicidial/admin/colors.php
+++ modern-vicidial/admin/colors.php
@@ -5,7 +5,7 @@
     $SSframe_background='FFF';
     $SSstd_row1_background='f2f2f2';
     $SSstd_row2_background='FFF';
-    $SSstd_row3_background='8EBCFD';
+    $SSstd_row3_background='d7e2f2';
     $SSstd_row4_background='FFF';
     $SSstd_row5_background='A3C3D6';
     $SSalt_row1_background='BDFFBD';
