1a27e3e82c6e18e72c815041498bd05c14fbce0e
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-11 15:10:22 -0400

    feat: Improve admin header

--- vicidial/admin_header.php
+++ vicidial/admin_header.php
@@ -303,7 +303,7 @@ if($short_header)
 	else
 		{
 		?>
-		<TABLE CELLPADDING=0 CELLSPACING=0 BGCOLOR=#<?php echo "$SSmenu_background" ?>><TR>
+		<div class="p-2" style="background-color:#<?=$SSmenu_background?>"><TABLE CELLPADDING=0 CELLSPACING=0 BGCOLOR=#<?php echo "$SSmenu_background" ?>><TR>
 		<TD><A HREF="./admin.php"><IMG SRC="<?php echo $selected_small_logo; ?>" WIDTH=71 HEIGHT=22 BORDER=0 ALT="System logo"></A> &nbsp; </TD>
 		<?php 
 		if ( ($reports_only_user < 1) and ($qc_only_user < 1) )
@@ -352,7 +352,7 @@ if($short_header)
 			}
 		?>
 		</TR>
-		</TABLE>
+		</TABLE></div>
 		<?php
 		}
 	}
