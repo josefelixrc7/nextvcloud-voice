c1374cda56c2cc60f30dc65c946ab8830399ef56
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-11 14:47:22 -0400

    feat: Improve admi head

--- modern-vicidial/admin/head.php
+++ modern-vicidial/admin/head.php
@@ -1,16 +1,12 @@
 <?php
 
-?>
-
-<!-- CSS files -->
+echo '<!-- CSS files -->
     <link href="/modern-vicidial/css/libraries/fontawesome-v5.9.0.min.css" rel="stylesheet">
     <link href="/modern-vicidial/css/libraries/bootstrap-v5.1.1/bootstrap.min.css" rel="stylesheet">
     <link href="/modern-vicidial/css/custom_admin.css" rel="stylesheet">
 <!-- /CSS files -->
 
 <script src="/modern-vicidial/js/libraries/bootstrap-v5.1.1/bootstrap.bundle.min.js"></script>
-<script src="/modern-vicidial/js/libraries/jquery.min-v3.4.1.js"></script>
-
-<?php
+<script src="/modern-vicidial/js/libraries/jquery.min-v3.4.1.js"></script>';
 
 ?>
