9236206b967882d1ce8effc7fda719ae2c3293d8
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-21 17:46:36 -0400

    feat: Improve footer

--- modern-vicidial/agc/vicidial/panel/main.php
+++ modern-vicidial/agc/vicidial/panel/main.php
@@ -118,27 +118,50 @@
                 <?php
                     include '../modern-vicidial/agc/vicidial/panel/actions.php';
                 ?>
-                <footer>
+                <footer class="mt-5">
                     <span id="MaiNfooterspan">
-                        <span id="blind_monitor_notice_span"><b><font color="red"> &nbsp; &nbsp; <span id="blind_monitor_notice_span_contents"></span></font></b></span>
-                        <table bgcolor="<?php echo $MAIN_COLOR ?>" id="MaiNfooter"><tr height="32px"><td height="32px"><font face="Arial,Helvetica" size="1"><?php echo _QXZ("VERSION:"); ?> <?php echo $version ?> &nbsp; <?php echo _QXZ("BUILD:"); ?> <?php echo $build ?> &nbsp; &nbsp; <?php echo _QXZ("Server:"); ?> <?php echo $server_ip ?>  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</font><br />
-                        <font class="body_small">
-                        <span id="busycallsdisplay"><a href="#"  onclick="conf_channels_detail('SHOW');"><?php echo _QXZ("Show conference call channel information"); ?></a>
-                        <br /><br />&nbsp;</span></font></td><td align="right" height="32px">
-                        </td></tr>
-                        <tr><td colspan="3"><span id="outboundcallsspan"></span></td></tr>
-                        <tr><td colspan="3"><font class="body_small"><span id="AgentAlertSpan">
-                        <?php
-                        if ( (preg_match('/ON/',$VU_alert_enabled)) and ($AgentAlert_allowed > 0) )
-                            {echo "<a href=\"#\" onclick=\"alert_control('OFF');return false;\">"._QXZ("Alert is ON")."</a>";}
-                        else
-                            {echo "<a href=\"#\" onclick=\"alert_control('ON');return false;\">"._QXZ("Alert is OFF")."</a>";}
-                        ?>
-                        </span></font></td></tr>
-                        <tr><td colspan="3">
-                        <font class="body_small">
-                        </font>
-                        </td></tr></table>
+                        <span id="blind_monitor_notice_span">
+                            <b>
+                                <font color="red"> &nbsp; &nbsp;
+                                    <span id="blind_monitor_notice_span_contents"></span>
+                                </font>
+                            </b>
+                        </span>
+                        <table id="MaiNfooter">
+                            <tr>
+                                <td>
+                                    <?php echo _QXZ("VERSION:"); ?>
+                                    <?php echo $version ?> &nbsp; <?php echo _QXZ("BUILD:"); ?>
+                                    <?php echo $build ?> &nbsp; <?php echo _QXZ("Server:"); ?>
+                                    <?php echo $server_ip ?>
+                                    
+                                    <span id="busycallsdisplay">
+                                        <a class=\"btn btn-sm btn-outline-secondary\" href="#"  onclick="conf_channels_detail('SHOW');">
+                                            <?php echo _QXZ("Show conference call channel information"); ?>
+                                        </a>
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="3">
+                                    <span id="outboundcallsspan"></span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="3">
+                                    <font class="body_small">
+                                        <span id="AgentAlertSpan">
+                                            <?php
+                                            if ( (preg_match('/ON/',$VU_alert_enabled)) and ($AgentAlert_allowed > 0) )
+                                                {echo "<a class=\"btn btn-sm btn-outline-secondary\" href=\"#\" onclick=\"alert_control('OFF');return false;\">"._QXZ("Alert is ON")."</a>";}
+                                            else
+                                                {echo "<a class=\"btn btn-sm btn-outline-secondary\" href=\"#\" onclick=\"alert_control('ON');return false;\">"._QXZ("Alert is OFF")."</a>";}
+                                            ?>
+                                        </span>
+                                    </font>
+                                </td>
+                            </tr>
+                        </table>
                     </span>
                 </footer>
 
