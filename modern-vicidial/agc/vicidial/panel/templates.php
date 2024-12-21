f0a1fb7f64af20b594e31108f756c978085dc073
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-21 18:12:20 -0400

    feat: add basic templates

---
+++ modern-vicidial/agc/vicidial/panel/templates.php
@@ -0,0 +1,28 @@
+
+<span class="w-100 h-100 bg-white" style="position:fixed;left:0px;top:0px;z-index:<?php $zi++; echo $zi ?>;" id="SysteMDisablEBoX">
+	<div class="text-center mt-5">
+        
+	</div>
+</span>
+
+<span class="w-100 h-100 overflow-scroll" tabindex="-1" style="background:rgba(0, 0, 0, 0.5);position:fixed;left:0px;top:0px;z-index:<?php $zi++; echo $zi ?>;" id="CalLLoGDisplaYBox">
+    <div class="w-100 mx-auto mt-2">
+
+		<div class="card">
+			<div class="card-header">
+				<h5 class="d-flex">
+					<span class="w-auto"><?php echo _QXZ("AGENT CALL LOG:"); ?></span>
+					<a class="btn btn-close ms-auto" href="#" onclick="CalLLoGVieWClose();return false;"></a>
+				</h5>
+			</div>
+			<div class="card-body">
+				
+			</div>
+			<div class="card-footer">
+				
+			</div>
+			
+		</div>
+
+	</div>
+</span>	
