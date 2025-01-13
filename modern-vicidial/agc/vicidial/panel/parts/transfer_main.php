f76cdc4d33a058c654f508b262d4711db846d0fc
Jose F Rivas C <josefelixrc7@gmail.com>
2025-01-13 14:34:49 -0400

    feat: Improve transfer main

--- modern-vicidial/agc/vicidial/panel/parts/transfer_main.php
+++ modern-vicidial/agc/vicidial/panel/parts/transfer_main.php
@@ -11,7 +11,7 @@
 			</div>
 			<div class="card-body">
                     
-                <table bgcolor="#CCCCFF" width="<?php echo $SDwidth ?>px">
+                <table class="table">
                     <tr valign="top">
                         <td align="left" height="30px">
                             <span class="text_input" id="TransferMaindiv">
@@ -24,7 +24,7 @@
                                         <tr>
                                             <td align="left" colspan="3">
                                                 <span id="XfeRGrouPLisT">
-                                                    <select size="1" name="XfeRGrouP" id="XfeRGrouP" class="cust_form" onChange="XferAgentSelectLink();return false;">
+                                                    <select size="1" name="XfeRGrouP" id="XfeRGrouP" class="form-select" onChange="XferAgentSelectLink();return false;">
                                                         <option>-- <?php echo _QXZ("SELECT A GROUP TO SEND YOUR CALL TO"); ?> --</option>
                                                     </select>
                                                 </span>
@@ -45,10 +45,10 @@
                                         <tr>
                                             <td align="left" colspan="2">
                                                 <img src="./images/<?php echo _QXZ("vdc_XB_seconds.gif"); ?>" border="0" alt="seconds" style="vertical-align:middle" />
-                                                <input type="text" size="2" name="xferlength" id="xferlength" maxlength="4" class="cust_form" readonly="readonly" />
+                                                <input type="text" size="2" name="xferlength" id="xferlength" maxlength="4" class="form-control" readonly="readonly" />
                                                 &nbsp; 
                                                 <img src="./images/<?php echo _QXZ("vdc_XB_channel.gif"); ?>" border="0" alt="channel" style="vertical-align:middle" />
-                                                <input type="text" size="12" name="xferchannel" id="xferchannel" maxlength="200" class="cust_form" readonly="readonly" />
+                                                <input type="text" size="12" name="xferchannel" id="xferchannel" maxlength="200" class="form-control" readonly="readonly" />
                                             </td>
                                             <td align="left">
                                                 <span id="consultative_checkbox">
@@ -74,20 +74,31 @@
                                                 else
                                                 {
                                                     ?>
-                                                    <input type="text" size="20" name="xfernumber" id="xfernumber" maxlength="25" class="cust_form" value="<?php echo $preset_populate ?>" /> &nbsp;
+                                                    <input type="text" size="20" name="xfernumber" id="xfernumber" maxlength="25" class="form-control" value="<?php echo $preset_populate ?>" /> &nbsp;
                                                     <?php
                                                 }
                                                 ?>
-                                                <span id="agentdirectlink"><font class="body_small_bold"><a href="#" onclick="XferAgentSelectLaunch();return false;"><?php echo _QXZ("AGENTS"); ?></a></font></span>
+                                                <span id="agentdirectlink">
+                                                    <font class="body_small_bold">
+                                                        <a class="btn btn-primary" href="#" onclick="XferAgentSelectLaunch();return false;"><?php echo _QXZ("AGENTS"); ?></a>
+                                                    </font>
+                                                </span>
                                                 <input type="hidden" name="xferuniqueid" id="xferuniqueid" />
                                                 <input type="hidden" name="xfername" id="xfername" />
                                                 <input type="hidden" name="xfernumhidden" id="xfernumhidden" />
                                             </td>
                                             <td align="left">
-                                                <span id="dialoverride_checkbox"><input type="checkbox" name="xferoverride" id="xferoverride" size="1" value="0"><font class="body_tiny" /> <?php echo _QXZ("DIAL OVERRIDE"); ?>	<?php if ($manual_dial_override_field == 'DISABLED'){echo " "._QXZ("DISABLED");}?></font></span>
+                                                <span id="dialoverride_checkbox">
+                                                    <input type="checkbox" name="xferoverride" id="xferoverride" size="1" value="0">
+                                                    <font class="body_tiny" /> <?php echo _QXZ("DIAL OVERRIDE"); ?>	<?php if ($manual_dial_override_field == 'DISABLED'){echo " "._QXZ("DISABLED");}?></font>
+                                                </span>
                                             </td>
                                             <td align="left">
-                                                <span style="background-color: <?php echo $MAIN_COLOR ?>" id="Leave3WayCall"><a href="#" onclick="leave_3way_call('FIRST','YES');return false;"><img src="./images/<?php echo _QXZ("vdc_XB_leave3waycall.gif"); ?>" border="0" alt="LEAVE 3-WAY CALL" style="vertical-align:middle" /></a></span>
+                                                <span style="background-color: <?php echo $MAIN_COLOR ?>" id="Leave3WayCall">
+                                                    <a class="btn btn-primary"  href="#" onclick="leave_3way_call('FIRST','YES');return false;">
+                                                        <img src="./images/<?php echo _QXZ("vdc_XB_leave3waycall.gif"); ?>" border="0" alt="LEAVE 3-WAY CALL" style="vertical-align:middle" />
+                                                    </a>
+                                                </span>
                                             </td>
                                         </tr>
 
@@ -98,13 +109,13 @@
                                                 </span>
                                                 &nbsp;
                                                 <span style="background-color: <?php echo $MAIN_COLOR ?>" id="DialWithCustomer">
-                                                    <a href="#" onclick="SendManualDial('YES','YES');return false;">
+                                                    <a class="btn btn-primary" href="#" onclick="SendManualDial('YES','YES');return false;">
                                                         <img src="./images/<?php echo _QXZ("vdc_XB_dialwithcustomer.gif"); ?>" border="0" alt="Dial With Customer" style="vertical-align:middle" />
                                                     </a>
                                                 </span>
                                                 &nbsp;
                                                 <span style="background-color: <?php echo $MAIN_COLOR ?>" id="ParkCustomerDial">
-                                                    <a href="#" onclick="xfer_park_dial('YES');return false;">
+                                                    <a class="btn btn-primary" href="#" onclick="xfer_park_dial('YES');return false;">
                                                         <img src="./images/<?php echo _QXZ("vdc_XB_parkcustomerdial.gif"); ?>" border="0" alt="Park Customer Dial" style="vertical-align:middle" />
                                                     </a>
                                                 </span>
@@ -113,7 +124,9 @@
                                                 if ($enable_xfer_presets=='ENABLED')
                                                 {
                                                     ?>
-                                                    <span style="background-color: <?php echo $MAIN_COLOR ?>" id="PresetPullDown"><a href="#" onclick="generate_presets_pulldown('YES');return false;"><img src="./images/<?php echo _QXZ("vdc_XB_presetsbutton.gif"); ?>" border="0" alt="Presets Button" style="vertical-align:middle" /></a></span>
+                                                    <span style="background-color: <?php echo $MAIN_COLOR ?>" id="PresetPullDown">
+                                                        <a class="btn btn-primary"  href="#" onclick="generate_presets_pulldown('YES');return false;"><img src="./images/<?php echo _QXZ("vdc_XB_presetsbutton.gif"); ?>" border="0" alt="Presets Button" style="vertical-align:middle" /></a>
+                                                    </span>
                                                     <?php
                                                 }
                                                 else
@@ -121,25 +134,29 @@
                                                     if ( ($enable_xfer_presets=='CONTACTS') and ($VU_preset_contact_search != 'DISABLED') )
                                                     {
                                                         ?>
-                                                        <span style="background-color: <?php echo $MAIN_COLOR ?>" id="ContactPullDown"><a href="#" onclick="generate_contacts_search('YES');return false;"><img src="./images/<?php echo _QXZ("vdc_XB_contactsbutton.gif"); ?>" border="0" alt="Contacts Button" style="vertical-align:middle" /></a></span>
+                                                        <span style="background-color: <?php echo $MAIN_COLOR ?>" id="ContactPullDown">
+                                                            <a class="btn btn-primary"  href="#" onclick="generate_contacts_search('YES');return false;"><img src="./images/<?php echo _QXZ("vdc_XB_contactsbutton.gif"); ?>" border="0" alt="Contacts Button" style="vertical-align:middle" /></a>
+                                                        </span>
                                                         <?php
                                                     }
                                                     else
                                                     {
                                                         ?>
                                                         <font class="body_tiny">
-                                                        <a href="#" onclick="DtMf_PreSet_a();return false;">D1</a> 
-                                                        <a href="#" onclick="DtMf_PreSet_b();return false;">D2</a>
-                                                        <a href="#" onclick="DtMf_PreSet_c();return false;">D3</a>
-                                                        <a href="#" onclick="DtMf_PreSet_d();return false;">D4</a>
-                                                        <a href="#" onclick="DtMf_PreSet_e();return false;">D5</a>
+                                                        <a class="btn btn-secondary" href="#" onclick="DtMf_PreSet_a();return false;">D1</a> 
+                                                        <a class="btn btn-secondary" href="#" onclick="DtMf_PreSet_b();return false;">D2</a>
+                                                        <a class="btn btn-secondary" href="#" onclick="DtMf_PreSet_c();return false;">D3</a>
+                                                        <a class="btn btn-secondary" href="#" onclick="DtMf_PreSet_d();return false;">D4</a>
+                                                        <a class="btn btn-secondary" href="#" onclick="DtMf_PreSet_e();return false;">D5</a>
                                                         </font>
                                                         <?php
                                                     }
                                                 }
                                                 ?>
                                                 &nbsp;
-                                                <span style="background-color: <?php echo $MAIN_COLOR ?>" id="DialBlindVMail"><img src="./images/<?php echo _QXZ("vdc_XB_ammessage_OFF.gif"); ?>" border="0" alt="Blind Transfer VMail Message" style="vertical-align:middle" /></span>
+                                                <span style="background-color: <?php echo $MAIN_COLOR ?>" id="DialBlindVMail">
+                                                    <img src="./images/<?php echo _QXZ("vdc_XB_ammessage_OFF.gif"); ?>" border="0" alt="Blind Transfer VMail Message" style="vertical-align:middle" />
+                                                </span>
                                             </td>
                                         </tr>
 
@@ -150,7 +167,7 @@
                         </td>
                     </tr>
                 </table>
-                
+
             </div>
 			<div class="card-footer">
 				
